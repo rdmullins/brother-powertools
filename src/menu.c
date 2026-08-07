1c2a48bc5b45277965d9f43fccc7b1ccd2d93546
Roger Mullins <rogermullins.mba@gmail.com>
2026-08-06 20:54:51 -0400

    Menu prototype

--- src/menu.c
+++ src/menu.c
@@ -1,1 +1,15 @@
+#include <stdio.h>
 
+int main (void)
+{
+    int a = 0;
+    
+    printf("Brother PowerTools\n\n");
+    printf("1. Send Text\n");
+    printf("2. Receive Text\n");
+    printf("3. Exit\n\n");
+    printf("Enter your choice: ");
+    scanf("%d", &a);
+    printf("\nYou selected option %d\n", a);
+    return 0;
+}
