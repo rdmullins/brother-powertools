62b5853be793bde5293f06f17ec4ca974bc6622f
Roger Mullins <rogermullins.mba@gmail.com>
2026-08-07 09:10:54 -0400

    Created interactive menu

--- src/menu.c
+++ src/menu.c
@@ -2,14 +2,43 @@
 
 int main (void)
 {
-    int a = 0;
+    int choice = 0;
+
+    while (1) {
     
     printf("Brother PowerTools\n\n");
     printf("1. Send Text\n");
     printf("2. Receive Text\n");
-    printf("3. Exit\n\n");
+    printf("3. BASIC\n");
+    printf("4. Settings\n");
+    printf("5. Exit\n\n");
     printf("Enter your choice: ");
-    scanf("%d", &a);
-    printf("\nYou selected option %d\n", a);
-    return 0;
+    
+    if (scanf("%d", &choice) != 1) {
+        printf("scanf failed!\n");
+        return 1;
+    };
+
+    switch (choice) {
+        case 1:
+            printf("You selected Send Text\n");
+            break;
+        case 2:
+            printf("You selected Receive Text\n");
+            break;
+        case 3:
+            printf("You selected BASIC\n");
+            break;
+        case 4:
+            printf("You selected Settings\n");
+            break;
+        case 5:
+            printf("Exiting...\n");
+            return 0;
+        default:
+            printf("Invalid option. Please try again.\n");
+        }
+  
+    }
+    return 0; 
 }
